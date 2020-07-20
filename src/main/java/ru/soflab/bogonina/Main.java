43e97dcac63782b99a770df2a24887ccc83bfc13
Bogonina Violetta <violettabogonina@yandex.ru>
2020-07-20 11:23:39 +0300

    changes 20/07 11:23

--- src/main/java/ru/soflab/bogonina/Main.java
+++ src/main/java/ru/soflab/bogonina/Main.java
@@ -4,11 +4,13 @@ import ru.soflab.bogonina.dictionary.Dictionary;
 import ru.soflab.bogonina.dictionary.DictionaryCreationFactory;
 import ru.soflab.bogonina.dictionary.Word;
 
+import java.util.List;
+
 public class Main {
     public static void main(String [] args) throws Exception {
         Dictionary dictionary = DictionaryCreationFactory.getInstance().getDictionary();
         for(Word word: dictionary.getWords()) {
-            System.out.println(word);
+            System.out.println(word.getId() + "." + word.getNatural() + "-" + word.getTranscription() + "-" + word.getTranslation());
         }
     }
 }
